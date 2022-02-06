34762fdf35ed49f6dfa688ab5cab6ab043d90494
Nestor Mendoza <nestirco@gmail.com>
2022-02-05 22:38:27 -0500

    adding mui package

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { alpha } from '@mui/material/styles';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box sx={{
+      width: 1,
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      bgcolor: 'primary.main'
+    }}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: { xs: 'column', md: 'row' },
+          alignItems: 'center',
+          bgcolor: 'background.paper',
+          overflow: 'hidden',
+          borderRadius: '12px',
+          boxShadow: 10,
+          fontWeight: 'bold',
+        }}
+      >
+        <Box
+          component="img"
+          sx={{
+            height: 233,
+            width: 350,
+            maxHeight: { xs: 233, md: 167 },
+            maxWidth: { xs: 350, md: 250 },
+          }}
+          alt="The house from the offer."
+          src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
+        />
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: { xs: 'center', md: 'flex-start' },
+            m: 3,
+            minWidth: { md: 350 },
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Box component="span" sx={{ fontSize: 16, mt: 1 }}>
+            Fortuna Project
+          </Box>
+          <Box component="span" sx={{ color: 'primary.main', fontSize: 22 }}>
+            $XXX,XXX,XXX
+          </Box>
+          <Box
+            sx={{
+              mt: 1.5,
+              p: 0.5,
+              backgroundColor: (theme) => alpha(theme.palette.primary.main, 0.1),
+              borderRadius: '5px',
+              color: 'primary.main',
+              fontWeight: 'medium',
+              display: 'flex',
+              fontSize: 12,
+              alignItems: 'center',
+              '& svg': {
+                fontSize: 21,
+                mr: 0.5,
+              },
+            }}
+          >
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
 }
-
-export default App;
